309dc8a18e7bdbc10f5efbc4b18cd4b65ed573e7
Sandy Meier <smeier@kdevelop.org>
2011-08-29 00:06:08 +0200

    neue versionsnummer 1.1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ Ext.namespace('BVApp','BVApp.views','BVApp.models','BVApp.templates','BVApp.util
 BVApp.Main = {
     ui: null,
     errorEMail: "bvapp@berlin-vegan.org",
-    version: "1.0.3",
+    version: "1.1.0",
     favoriteListStoreID: "favoriteListStore",
     favoriteStoreID: "favoriteStore",
     restaurantStoreID: "restaurantStore",
